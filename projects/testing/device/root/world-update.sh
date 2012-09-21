d7a4c9a20122bb75190b5f547f2478d10753a583
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-09-21 15:33:26 +0200

    Adding some test scripts.

---
+++ projects/testing/device/root/world-update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ ! `uname` = 'Minix' ]
+then
+	echo "This script should be copied to your minix installation and run in there. (exiting)"
+	exit 1
+fi
+
+set -x
+set -e
+cd /usr/src
+git reset --hard
+git pull
+#if we don't have a local checkout create one
+if ! git branch | grep master 2>&1 > /dev/null
+then
+	git checkout -t origin/master
+else 
+	git checkout master
+fi
+make clean world 
---
+++ projects/testing/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import random
+import os
+import unittest
+import xmlrunner
+
+class TestSequenceFunctions(unittest.TestCase):
+#    def setUp(self):
+	#os.system("rm -rf test.img")
+	#os.system("cp /opt/data/test.img test.img")
+
+#    def test_base_image_boots(self):
+#	self.value = os.system("./tests/device_boots --image test.img")
+#	self.assertEqual(self.value,0)
+
+    def test_update_world(self):
+	self.value = os.system("cp test.img test-update.img")
+	self.value = os.system("./tests/update_world --image test-update.img")
+	self.assertEqual(self.value,0)
+
+    def test_test_fail(self):
+	self.value = os.system("./tests/test_fail")
+	self.assertEqual(self.value, 0)
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
