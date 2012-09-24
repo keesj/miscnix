d4aaa25226b453bd6535f0e5e60d6c76f0b0a896
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-09-24 21:08:42 +0200

    Updated run_in_tmux

--- projects/testing/run_in_tmux.sh
+++ projects/testing/run_in_tmux.sh
@@ -8,16 +8,18 @@
 #create  script.sh
 echo "#!/bin/sh
 rm -rf done
-$@
+$@ 2>&1 > script.log
 echo \$? > done" > script.sh
 
 chmod +x script.sh
 
 rm -rf done
-tmux new-session -d -x 80 -y 25 ./script.sh
+unset TMUX
+tmux new-session -d -x 120 -y 60 ./script.sh
 while [ ! -f done ]
 do
 	sleep 1
 done
+cat script.log
 exit `cat done`
 
