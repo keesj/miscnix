6f44708952c04cb0e1c6adf3967fe54916d4c98b
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-09-25 11:59:23 +0200

    Improved version of run_in_tmux

--- projects/testing/run_in_tmux.sh
+++ projects/testing/run_in_tmux.sh
@@ -3,23 +3,46 @@
 # Create a new session without attaching to it
 # -d
 # 
+# Create a child process with a pid
+# let that child start tmux
+#
 
 
 #create  script.sh
 echo "#!/bin/sh
-rm -rf done
-$@ 2>&1 > script.log
-echo \$? > done" > script.sh
+(
+$@  2>&1 
+pid=\$?
+echo \$pid > done
+) >> script.log
+#
+# ensure tail -f does handle
+# the loop one more time.
+#
+echo >> script.log
+" > script.sh
 
 chmod +x script.sh
 
 rm -rf done
 unset TMUX
-tmux new-session -d -x 120 -y 60 ./script.sh
-while [ ! -f done ]
-do
-	sleep 1
-done
-cat script.log
-exit `cat done`
+rm -rf script.log
 
+tmux new-session -d -x 120 -y 60 ./script.sh
+
+#wait for the file to be there
+while [ ! -f script.log ]
+do
+	sleep .2
+done
+
+tail -f script.log | while read line
+do
+	if [ -f done ] 
+	then
+		killall tail
+		exit
+	fi
+	echo $line
+done
+exit `cat done`
