c237a394cf47a98b7b2c6f1fb9a386c5669a5da3
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-10-04 18:01:41 +0200

    Use the same test file to speedup running tests.

--- projects/testing/test.py
+++ projects/testing/test.py
@@ -5,29 +5,26 @@ import unittest
 import xmlrunner
 
 class BasicTests(unittest.TestCase):
-    def setUp(self):
-	os.system("rm -rf test.img")
-	os.system("rm -rf test-reports");
-	os.system("mkdir test-reports");
-	os.system("cp /opt/data/test.img test.img")
 
     def test_base_image_boots(self):
 	self.value = os.system("./tests/device_boots --image test.img")
 	self.assertEqual(self.value,0)
 
     def test_update_world(self):
-	self.value = os.system("rm -rf test-update.img")
-	self.value = os.system("cp test.img test-update.img")
-	self.value = os.system("../autoinstall/script/04-run-script.expect -- --image test-update.img --script ./tests/update_world")
+	self.value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/update_world")
 	self.assertEqual(self.value,0)
 
     def test_updated_image_boots(self):
-	self.value = os.system("./tests/device_boots --image test-update.img")
+	self.value = os.system("./tests/device_boots --image test.img")
 	self.assertEqual(self.value,0)
 
     def test_run_posix_tests(self):
-	self.value = os.system("../autoinstall/script/04-run-script.expect -- --image test-update.img --script ./tests/run_posix_tests")
+	self.value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/run_posix_tests")
 	self.assertEqual(self.value,0)
 
 if __name__ == '__main__':
+    os.system("rm -rf test.img")
+    os.system("cp /opt/data/test.img test.img")
+    os.system("rm -rf test-reports");
+    os.system("mkdir test-reports");
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
