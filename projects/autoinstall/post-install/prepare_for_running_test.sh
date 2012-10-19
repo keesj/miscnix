d57e8544d1e377421bddad4a2f6836cd2039d047
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-10-19 14:14:31 +0200

    Use git config to configure gerrit reviewer.

--- projects/autoinstall/post-install/prepare_for_running_test.sh
+++ projects/autoinstall/post-install/prepare_for_running_test.sh
@@ -11,4 +11,6 @@ $MYSCP -r device/* root@127.0.0.1:/
 $MYSSH pwd
 $MYSSH /usr/pkg/sbin/pkg_add packages/unittest-xml-reporting-1.3.2.tgz
 $MYSSH /usr/pkg/sbin/pkg_add packages/git-review-1.19.tgz
+$MYSSH git config --global --add gitreview.username "jenkins3" 
+
 #echo haha
