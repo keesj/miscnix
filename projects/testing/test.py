6b7bb7724d841069c15da3fce5d6e04b0b6a6bc9
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-11-23 10:37:42 +0100

    updated tests added a test to install the created iso.

--- projects/testing/test.py
+++ projects/testing/test.py
@@ -19,17 +19,22 @@ class BasicTests(unittest.TestCase):
 	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/update_world")
 	self.assertEqual(value,0)
 
-    def test_03_create_iso(self):
-	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/create_iso")
-	self.assertEqual(value,0)
-
-    def test_04_updated_image_boots(self):
+    def test_03_updated_image_boots(self):
 	value = os.system("./tests/device_boots --image test.img")
 	self.assertEqual(value,0)
 
-    def test_05_run_posix_tests(self):
+    def test_04_run_posix_tests(self):
 	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/run_posix_tests")
 	self.assertEqual(value,0)
+
+    def test_05_create_iso(self):
+	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/create_iso")
+	self.assertEqual(value,0)
+
+    def test_06_install_iso(self):
+	value = os.system("./tests/install_iso")
+	self.assertEqual(value,0)
+
 
 if __name__ == '__main__':
     os.system("rm -rf test.img")
