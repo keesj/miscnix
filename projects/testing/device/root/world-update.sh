96ec760848c4524f2aec2970849c2d5d9563e89b
Kees Jongenburger <kees.jongenburger@gmail.com>
2012-11-23 12:13:14 +0100

    Simplify world update script.

--- projects/testing/device/root/world-update.sh
+++ projects/testing/device/root/world-update.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
-if [ ! `uname` = 'Minix' ]
-then
-	echo "This script should be copied to your minix installation and run in there. (exiting)"
-	exit 1
-fi
+#if [ ! `uname` = 'Minix' ]
+#then
+#	echo "This script should be copied to your minix installation and run in there. (exiting)"
+#	exit 1
+#fi
 
 VERSION=master
-while [ $# -gt 0 ]
-do
-    case "$1" in
-	-version) VERSION="$2"; shift;;
-	*)  break;;	# terminate while loop
-    esac
-    shift
-done
+case "$1" in
+  -version) VERSION="$2";;
+   *)  break;;	# terminate while loop
+esac
 
 set -x
 cd /usr/src
-#git config --global user.name jenkins3
-#git config --global user.email jenkins@keesj.dds.nl
-git config --global --add gitreview.username "jenkins3"
-git review -s
-git review -d $VERSION
+
+if [ $VERSION = "master" ]
+then
+	git checkout master
+else
+	git config --global --add gitreview.username "jenkins3"
+	git review -s
+	git review -d $VERSION
+fi
 export PATH=/usr/bin:/bin:/usr/pkg/bin:/usr/local/bin:/sbin:/usr/sbin
-#/root/bin:/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/pkg/bin:/usr/pkg/sbin:/usr/pkg/X11R6/bin
-#make clean world 2>&1
-#make clean world 2>&1
 make build 2>&1
