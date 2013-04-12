fabdb7e752971fbf85d9f99517662daf8436c53c
Kees Jongenburger <keesj@minix3.org>
2013-04-12 22:22:52 +0200

    Determine the tcp port to use at runtime.

--- projects/testing/test.py
+++ projects/testing/test.py
@@ -5,43 +5,46 @@ import unittest
 import xmlrunner
 from time import sleep
 
+
 class BasicTests(unittest.TestCase):
     def setUp(self):
-        while os.system("netstat -na | grep LIST | grep tcp | grep 8888 1>/dev/null") == 0:
-            print "Waiting from port 8888 to be free"
+	self.port = 8989
+        while os.system("netstat -na | grep LIST | grep tcp | grep %s 2>/dev/null" % self.port) == 0:
+            print "port %s taken trying next " % self.port
+	    self.port += 1
 	    sleep(1)
 
     def test_01_base_image_boots(self):
-	value = os.system("./tests/device_boots --image test.img")
+	value = os.system("./tests/device_boots --image test.img -p %s" % self.port)
 	self.assertEqual(value,0)
 
     def test_02_update_world(self):
-	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/update_world")
+	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/update_world -p %s" % self.port)
 	self.assertEqual(value,0)
 
     def test_03_updated_image_boots(self):
-	value = os.system("./tests/device_boots --image test.img")
+	value = os.system("./tests/device_boots --image test.img -p %s" % self.port)
 	self.assertEqual(value,0)
 
     def test_04_run_posix_tests(self):
-	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/run_posix_tests")
+	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/run_posix_tests -p %s" % self.port)
 	self.assertEqual(value,0)
 
     def test_05_create_iso(self):
-	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/create_iso")
+	value = os.system("../autoinstall/script/04-run-script.expect -- --image test.img --script ./tests/create_iso -p %s" % self.port)
 	self.assertEqual(value,0)
 
     def test_06_install_iso(self):
-	value = os.system("./tests/install_iso")
+	value = os.system("./tests/install_iso -p %s" % self.port)
 	self.assertEqual(value,0)
 
     def test_07_installed_image_boots(self):
-	value = os.system("./tests/device_boots --image install_test.img")
+	value = os.system("./tests/device_boots --image install_test.img -p %s" % self.port )
 	self.assertEqual(value,0)
 
 if __name__ == '__main__':
-    os.system("rm -rf test.img")
-    os.system("cp /apts/data/test.img test.img")
-    os.system("rm -rf test-reports");
-    os.system("mkdir test-reports");
+    #os.system("rm -rf test.img")
+    #os.system("cp /apts/data/test.img test.img")
+    #os.system("rm -rf test-reports");
+    #os.system("mkdir test-reports");
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
